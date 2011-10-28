273764a27f5ddea2b86af4dedb404bbefa2e7e81
youngconsulting <niklas@youngconsulting.se>
2011-10-28 14:37:42 +0200

    Some changes 28/10

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,10 +2,19 @@
 ActiveAdmin.register User do
   index do
     column :id
-    column "Förnamn", :fname
-    column "Efternamn", :lname
-    column "Utbildning", :education
+    
+		column "Förnamn", :fname do |user|
+			link_to user.fname, admin_user_path(user)
+		end
+    
+		column "Efternamn", :lname do |user|
+			link_to user.lname, admin_user_path(user)
+		end
+
+		column "Födelsedag", :bday
+		column "Utbildning", :education
     column "Söker jobb inom", :looking_for
-    default_actions
+		column "E-mail", :email
+		default_actions
   end
 end
