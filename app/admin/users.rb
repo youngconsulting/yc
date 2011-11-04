9eb1e2b05f45d2a6700fa78534edcdf6d7b74b4a
Daniel <znowm4n@gmail.com>
2011-11-04 12:47:48 +0100

    updated views and admin users.rb

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,12 @@
 # encoding: UTF-8
 ActiveAdmin.register User do
+	
+	filter :fname
+	filter :lname 
+	filter :bday
+	filter :looking_for
+	filter :city
+
   index do
     column :id
     
@@ -13,8 +20,14 @@ ActiveAdmin.register User do
 
 		column "Födelsedag", :bday
 		column "Utbildning", :education
-    column "Söker jobb inom", :looking_for
+		column "Söker jobb inom", :looking_for
 		column "E-mail", :email
 		default_actions
   end
+
+	show do
+    attributes_table :fname, :lname, :bday, :address, :areacode, :city, :education, :info, :looking_for,
+    :tele, :cv_file_name  
+  end
+
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,10 @@
 class JobsController < InheritedResources::Base
   before_filter :authenticate_user!, :except => [:index, :show]
 
+  def index
+    @jobs = Job.order("updated_at DESC").all
+  end
+
   def add_job
     @job = Job.find(params[:id])
     current_user.jobs << @job
